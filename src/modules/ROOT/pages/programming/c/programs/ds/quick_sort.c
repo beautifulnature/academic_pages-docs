834df7301aa01cdc94ab34d39b664ad50b7008d8
beautifulnature <36604320+beautifulnature@users.noreply.github.com>
2022-01-03 16:57:43 +0530

    in progress

---
+++ src/modules/ROOT/pages/programming/c/programs/ds/quick_sort.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n, a[25];
+    printf("Enter total elements:\n");
+    scanf("%d", &n);
+    printf("enter elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    quicksort(a, 0, n - 1);
+    return 0;
+}
+
+void quicksort(int a[25], int first, int last)
+{
+    int i, j, pivot, temp;
+    if (first < last)
+    {
+        pivot = first;
+        i = first;
+        j = last;
+        while (i < j)
+        {
+            while (a[i] <= a[pivot] && i < last)
+            {
+                i++;
+            }
+            while (a[j] > a[pivot])
+            {
+                j--;
+            }
+            if (i < j)
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        temp = a[pivot];
+        a[pivot] = a[j];
+        a[j] = temp;
+        quicksort(a, first, j - 1);
+        quicksort(a, j + 1, last);
+    }
+
+}
